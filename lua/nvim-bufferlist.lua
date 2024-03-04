bbff6e5bcd68642c0a2dd7f594392c4dda1bacc1
kilavila <christer.kilavik@protonmail.com>
2024-03-04 20:53:15 +0100

    keymaps fix2

--- lua/nvim-bufferlist.lua
+++ lua/nvim-bufferlist.lua
@@ -92,16 +92,18 @@ local function set_mappings()
     ['<esc>'] = 'close_window()',
     ['<cr>'] = 'go_to_buffer()',
     q = 'close_window()',
-    k = 'move_cursor()'
   }
+
   for k, v in pairs(mappings) do
     api.nvim_buf_set_keymap(buf, 'n', k, ':lua require"nvim-bufferlist".' .. v .. '<cr>', {
       nowait = true, noremap = true, silent = true
     })
   end
+
   local other_chars = {
     'a', 'b', 'c', 'd', 'e', 'f', 'g', 'i', 'n', 'o', 'p', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
   }
+
   for k, v in ipairs(other_chars) do
     api.nvim_buf_set_keymap(buf, 'n', v, '', { nowait = true, noremap = true, silent = true })
     api.nvim_buf_set_keymap(buf, 'n', v:upper(), '', { nowait = true, noremap = true, silent = true })
