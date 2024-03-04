4b576215d70ee0d81a389dbcdb4d914d78e9cc80
kilavila <christer.kilavik@protonmail.com>
2024-03-04 20:44:29 +0100

    init testing

---
+++ lua/nvim-bufferlist.lua
@@ -0,0 +1,127 @@
+local api = vim.api
+local buf, win
+
+local function center(str)
+  local width = api.nvim_win_get_width(0)
+  local shift = math.floor(width / 2) - math.floor(string.len(str) / 2)
+  return string.rep(' ', shift) .. str
+end
+
+local function open_window()
+  buf = api.nvim_create_buf(false, true)
+  local border_buf = api.nvim_create_buf(false, true)
+
+  api.nvim_buf_set_option(buf, 'bufhidden', 'wipe')
+  api.nvim_buf_set_option(buf, 'filetype', 'bufferlist')
+
+  local width = api.nvim_get_option("columns")
+  local height = api.nvim_get_option("lines")
+
+  local win_height = math.ceil(height * 0.8 - 4)
+  local win_width = math.ceil(width * 0.8)
+  local row = math.ceil((height - win_height) / 2 - 1)
+  local col = math.ceil((width - win_width) / 2)
+
+  local border_opts = {
+    style = 'minimal',
+    relative = 'editor',
+    width = win_width + 2,
+    height = win_height + 2,
+    row = row - 1,
+    col = col - 1
+  }
+
+  local opts = {
+    style = 'minimal',
+    relative = 'editor',
+    width = win_width,
+    height = win_height,
+    row = row,
+    col = col
+  }
+
+  local border_lines = { '╔' .. string.rep('═', win_width) .. '╗' }
+  local middle_line = '║' .. string.rep(' ', win_width) .. '║'
+  for i = 1, win_height do
+    table.insert(border_lines, middle_line)
+  end
+  table.insert(border_lines, '╚' .. string.rep('═', win_width) .. '╝')
+  api.nvim_buf_set_lines(border_buf, 0, -1, false, border_lines)
+
+  local border_win = api.nvim_open_win(border_buf, true, border_opts)
+  win = api.nvim_open_win(buf, true, opts)
+  api.nvim_command('au BufWipeout <buffer> exe "silent bwipeout! "' .. border_buf)
+
+  api.nvim_win_set_option(win, 'cursorline', true)
+
+  api.nvim_buf_set_lines(buf, 0, -1, false, { center('Open buffers'), '', '' })
+  api.nvim_buf_add_highlight(buf, -1, 'BufferListHeader', 0, 0, -1)
+end
+
+local function update_view()
+  api.nvim_buf_set_option(buf, 'modifiable', true)
+  local buffers = vim.fn.getbufinfo({ buflisted = 1 })
+
+  local result = {}
+  for _, buffer in ipairs(buffers) do
+    table.insert(result, center(buffer.name))
+  end
+  api.nvim_buf_set_lines(buf, 2, -1, false, result)
+  api.nvim_buf_set_option(buf, 'modifiable', false)
+end
+
+local function close_window()
+  api.nvim_win_close(win, true)
+end
+
+local function go_to_buffer()
+  -- get selected buffer from bufferlist
+  local line = api.nvim_get_current_line()
+  local bufnr = string.match(line, '%d+')
+  close_window()
+  api.nvim_command('b ' .. bufnr)
+end
+
+local function move_cursor()
+  local new_pos = math.max(4, api.nvim_win_get_cursor(win)[1] - 1)
+  api.nvim_win_set_cursor(win, { new_pos, 0 })
+end
+
+local function set_mappings()
+  local mappings = {
+    ['<esc>'] = 'close_window()',
+    ['<cr>'] = 'go_to_buffer()',
+    o = 'go_to_buffer()',
+    l = 'go_to_buffer()',
+    q = 'close_window()',
+    k = 'move_cursor()'
+  }
+  for k, v in pairs(mappings) do
+    api.nvim_buf_set_keymap(buf, 'n', k, ':lua require"whid".' .. v .. '<cr>', {
+      nowait = true, noremap = true, silent = true
+    })
+  end
+  local other_chars = {
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'i', 'n', 'o', 'p', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+  }
+  for k, v in ipairs(other_chars) do
+    api.nvim_buf_set_keymap(buf, 'n', v, '', { nowait = true, noremap = true, silent = true })
+    api.nvim_buf_set_keymap(buf, 'n', v:upper(), '', { nowait = true, noremap = true, silent = true })
+    api.nvim_buf_set_keymap(buf, 'n', '<c-' .. v .. '>', '', { nowait = true, noremap = true, silent = true })
+  end
+end
+
+local function bufferlist()
+  open_window()
+  set_mappings()
+  update_view()
+  api.nvim_win_set_cursor(win, { 4, 0 })
+end
+
+return {
+  bufferlist = bufferlist,
+  update_view = update_view,
+  open_file = go_to_buffer,
+  move_cursor = move_cursor,
+  close_window = close_window
+}
---
+++ plugin/nvim-bufferlist.vim
@@ -0,0 +1,13 @@
+if exists('g:loaded_nvim_bufferlist') | finish | endif
+
+let s:save_cpo = &cpo
+set cpo&vim
+
+hi def link BufferListHeader      Number
+
+command! BufferList lua require'nvim-bufferlist'.bufferlist()
+
+let &cpo = s:save_cpo
+unlet s:save_cpo
+
+let g:loaded_nvim_bufferlist = 1
