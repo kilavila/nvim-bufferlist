9fafa4f7d2dffc80d1fc732aa7db31f28d05e493
kilavila <christer.kilavik@protonmail.com>
2024-03-06 11:34:25 +0100

    quicknav init

---
+++ lua/nvim-quicknav.lua
@@ -0,0 +1,125 @@
+local api = vim.api
+local buf, win
+
+local pinned_files = {}
+
+local function open_window()
+  buf = api.nvim_create_buf(false, true)
+  local border_buf = api.nvim_create_buf(false, true)
+
+  api.nvim_buf_set_option(buf, 'bufhidden', 'wipe')
+  api.nvim_buf_set_option(buf, 'filetype', 'bufferlist')
+
+  local width = api.nvim_get_option("columns")
+  local height = api.nvim_get_option("lines")
+
+  local win_height = math.ceil(height * 0.5 - 4)
+  local win_width = math.ceil(width * 0.4)
+  local row = math.ceil((height - win_height) / 2 - 1)
+  local col = math.ceil((width - win_width) / 2)
+
+  local border_opts = {
+    style = 'minimal',
+    relative = 'editor',
+    width = win_width + 2,
+    height = win_height + 2,
+    row = row - 1,
+    col = col - 1
+  }
+
+  local opts = {
+    style = 'minimal',
+    relative = 'editor',
+    width = win_width,
+    height = win_height,
+    row = row,
+    col = col
+  }
+
+  local border_title = ' Quick Navigation '
+  local border_lines = { '╭' .. border_title .. string.rep('─', win_width - string.len(border_title)) .. '╮' }
+  local middle_line = '│' .. string.rep(' ', win_width) .. '│'
+  for _ = 1, win_height do
+    table.insert(border_lines, middle_line)
+  end
+  table.insert(border_lines, '╰' .. string.rep('─', win_width) .. '╯')
+  api.nvim_buf_set_lines(border_buf, 0, -1, false, border_lines)
+
+  local border_win = api.nvim_open_win(border_buf, true, border_opts)
+  win = api.nvim_open_win(buf, true, opts)
+  api.nvim_command('au BufWipeout <buffer> exe "silent bwipeout! "' .. border_buf)
+
+  api.nvim_win_set_option(win, 'cursorline', true)
+end
+
+local function add_current_file()
+  local current_file = api.nvim_buf_get_name(0)
+  table.insert(pinned_files, current_file)
+end
+
+local function update_view()
+  api.nvim_buf_set_option(buf, 'modifiable', true)
+
+  local result = {}
+  for _, file in ipairs(pinned_files) do
+    table.insert(pinned_files, file)
+  end
+
+  api.nvim_buf_set_lines(buf, 0, -1, false, result)
+  api.nvim_buf_set_option(buf, 'modifiable', true)
+end
+
+local function close_window()
+  api.nvim_win_close(win, true)
+end
+
+local function go_to_file()
+  local selected_line = api.nvim_get_current_line()
+
+  close_window()
+  api.nvim_command('tabnew ' .. selected_line)
+end
+
+local function move_cursor()
+  local new_pos = math.max(4, api.nvim_win_get_cursor(win)[1] - 1)
+  api.nvim_win_set_cursor(win, { new_pos, 0 })
+end
+
+local function set_mappings()
+  local mappings = {
+    ['<esc>'] = 'close_window()',
+    ['<cr>'] = 'go_to_file()',
+  }
+
+  for k, v in pairs(mappings) do
+    api.nvim_buf_set_keymap(buf, 'n', k, ':lua require"nvim-quicknav".' .. v .. '<cr>', {
+      nowait = true, noremap = true, silent = true
+    })
+  end
+
+  --[[ local other_chars = {
+    'a', 'b', 'c', 'e', 'f', 'g', 'i', 'n', 'o', 'p', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+  }
+
+  for _, v in ipairs(other_chars) do
+    api.nvim_buf_set_keymap(buf, 'n', v, '', { nowait = true, noremap = true, silent = true })
+    api.nvim_buf_set_keymap(buf, 'n', v:upper(), '', { nowait = true, noremap = true, silent = true })
+    api.nvim_buf_set_keymap(buf, 'n', '<c-' .. v .. '>', '', { nowait = true, noremap = true, silent = true })
+  end ]]
+end
+
+local function quicknav()
+  open_window()
+  update_view()
+  set_mappings()
+  api.nvim_win_set_cursor(win, { 1, 0 })
+end
+
+return {
+  quicknav = quicknav,
+  add_current_file = add_current_file,
+  update_view = update_view,
+  go_to_file = go_to_file,
+  move_cursor = move_cursor,
+  close_window = close_window
+}
--- plugin/nvim-bufferlist.vim
+++ plugin/nvim-bufferlist.vim
@@ -8,6 +8,8 @@ hi def link BufferListHeader      Number
 command! BufferListOpen lua require'nvim-bufferlist'.bufferlist()
 command! BufferListClose lua require'nvim-bufferlist'.close_window()
 command! BufferListUpdate lua require'nvim-bufferlist'.update_view()
+command! QuickNavOpen lua require'nvim-quicknav'.quicknav()
+command! QuickNavAdd lua require'nvim-quicknav'.add_current_file()
 
 let &cpo = s:save_cpo
 unlet s:save_cpo
